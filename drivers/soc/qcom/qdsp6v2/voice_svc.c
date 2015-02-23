8699279f0058d3eb54c9f427111bd1881316c91d
Karthik Reddy Katta <a_katta@codeaurora.org>
2015-02-23 14:10:20 +0530

    voice_svc : Fix crash in voice_svc driver during SSR
    Current implementation allocates invalid amount of
    memory for payload when RESET_EVENTS is received.
    Set the payload size to zero. Avoid allocating
    and accessing the payload memory for RESET_EVENTS.
    
    CRs-fixed: 798906
    Change-Id: I5c07785d516d2f64d9bd183d3b91b8cf069f43d5
    Signed-off-by: Karthik Reddy Katta <a_katta@codeaurora.org>

--- drivers/soc/qcom/qdsp6v2/voice_svc.c
+++ drivers/soc/qcom/qdsp6v2/voice_svc.c
@@ -103,6 +103,13 @@ static int32_t qdsp_apr_callback(struct apr_client_data *data, void *priv)
 		} else if (data->reset_proc == APR_DEST_MODEM) {
 			pr_debug("%s: Received Modem reset event\n", __func__);
 		}
+		/* Set the remaining member variables to default values
+			for RESET_EVENTS */
+		data->payload_size = 0;
+		data->payload = NULL;
+		data->src_port = 0;
+		data->dest_port = 0;
+		data->token = 0;
 	}
 
 	spin_lock_irqsave(&prtd->response_lock, spin_flags);
