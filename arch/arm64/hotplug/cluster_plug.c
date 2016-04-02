68170ac71171c8924c66c7f5ae51e955c9c86192
Christopher R. Palmer <crpalmer@gmail.com>
2016-04-02 10:57:00 -0400

    cluster-plug: Ensure the clusters are online when we enter !active
    Change-Id: I3e66a0b6267b0626c6d0421d143222477adec5d0

--- arch/arm64/hotplug/cluster_plug.c
+++ arch/arm64/hotplug/cluster_plug.c
@@ -258,19 +258,22 @@ static void cluster_plug_perform(void)
 
 static void __ref cluster_plug_work_fn(struct work_struct *work)
 {
-	if (!interactive) {
-		enable_little_cluster();
-		disable_big_cluster();
-		/* Do not schedule more work */
-		return;
-	}
-
-	if (interactive && !big_cluster_enabled)
-		enable_big_cluster();
-
 	if (active) {
+		if (!interactive) {
+			enable_little_cluster();
+			disable_big_cluster();
+			/* Do not schedule more work */
+			return;
+		}
+
+		if (interactive && !big_cluster_enabled)
+			enable_big_cluster();
+
 		cluster_plug_perform();
 		queue_clusterplug_work(sampling_time);
+	} else {
+		enable_big_cluster();
+		enable_little_cluster();
 	}
 }
 
