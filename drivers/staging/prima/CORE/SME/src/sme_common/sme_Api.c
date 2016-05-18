3e4c36c6b562771e2a72252af266299cf10c1ed5
Gupta, Kapil <kapgupta@codeaurora.org>
2016-05-18 12:09:13 +0530

    wlan: Fix memory leak issue
    sme_SetSignificantChange, sme_ReSetSignificantChange API doesn't
    free memory allocated in case of error.
    This fix takes care of issues.
    
    Change-Id: Ie524cc6cfdc19d1cf532a1f5aac0df5bff578fe3
    CRs-Fixed: 1017855

--- drivers/staging/prima/CORE/SME/src/sme_common/sme_Api.c
+++ drivers/staging/prima/CORE/SME/src/sme_common/sme_Api.c
@@ -12723,11 +12723,19 @@ eHalStatus sme_SetSignificantChange (tHalHandle hHal,
         MTRACE(vos_trace(VOS_MODULE_ID_SME,
                  TRACE_CODE_SME_TX_WDA_MSG, NO_SESSION, vosMessage.type));
         vosStatus = vos_mq_post_message(VOS_MQ_ID_WDA, &vosMessage);
-        if (!VOS_IS_STATUS_SUCCESS(vosStatus))
+        if (!VOS_IS_STATUS_SUCCESS(vosStatus)) {
+           vos_mem_free(pEXTScanSetSignificantReq);
            status = eHAL_STATUS_FAILURE;
+        }
 
         sme_ReleaseGlobalLock(&pMac->sme);
+    } else {
+        VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_ERROR, "%s: "
+                "sme_AcquireGlobalLock error", __func__);
+        vos_mem_free(pEXTScanSetSignificantReq);
+        status = eHAL_STATUS_FAILURE;
     }
+
     return(status);
 }
 
@@ -12769,10 +12777,17 @@ eHalStatus sme_ResetSignificantChange (tHalHandle hHal,
         MTRACE(vos_trace(VOS_MODULE_ID_SME,
                  TRACE_CODE_SME_TX_WDA_MSG, NO_SESSION, vosMessage.type));
         vosStatus = vos_mq_post_message(VOS_MQ_ID_WDA, &vosMessage);
-        if (!VOS_IS_STATUS_SUCCESS(vosStatus))
+        if (!VOS_IS_STATUS_SUCCESS(vosStatus)) {
+           vos_mem_free(pEXTScanResetSignificantReq);
            status = eHAL_STATUS_FAILURE;
+        }
 
         sme_ReleaseGlobalLock(&pMac->sme);
+    } else {
+        VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_ERROR, "%s: "
+                "sme_AcquireGlobalLock error", __func__);
+        vos_mem_free(pEXTScanResetSignificantReq);
+        status = eHAL_STATUS_FAILURE;
     }
     return(status);
 }
