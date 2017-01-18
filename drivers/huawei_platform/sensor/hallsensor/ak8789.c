49aa16fc516e618c56629d28c0096dea167618d2
Christopher R. Palmer <crpalmer@gmail.com>
2017-01-18 21:34:00 +0100

    kiwi: Make the hall sensor generate SW_LID on flip cover events
    Change-Id: I2e09c25cdc1226e20ab14ac161dec684cd4279a7

--- drivers/huawei_platform/sensor/hallsensor/ak8789.c
+++ drivers/huawei_platform/sensor/hallsensor/ak8789.c
@@ -485,7 +485,7 @@ void hall_work_func(struct work_struct *work)
 	value = query_hall_event();
 	if((camera_hall_support_is_true == true) && ((value == 0x10) || (value == 0x20)))
 		report_overturn_num += 1;
-	input_event(hw_hall_dev.hw_input_hall, EV_MSC, MSC_SCAN, value);
+	input_report_switch(hw_hall_dev.hw_input_hall, SW_LID, value & 0x1);
 	input_sync(hw_hall_dev.hw_input_hall);
 	atomic_dec(&irq_no_at);
 	AK8789_WARNMSG("input hall event:0x%x",value);
@@ -888,7 +888,10 @@ int hall_pf_probe(struct platform_device *pdev)
 	}
 	hw_hall_dev.hw_input_hall->name = "hall";
 	set_bit(EV_MSC, hw_hall_dev.hw_input_hall->evbit);
+	set_bit(EV_SW, hw_hall_dev.hw_input_hall->evbit);
 	set_bit(MSC_SCAN, hw_hall_dev.hw_input_hall->mscbit);
+
+	input_set_capability(hw_hall_dev.hw_input_hall, EV_SW, SW_LID);
 
 	err = input_register_device(hw_hall_dev.hw_input_hall);
 	if (err){
@@ -901,6 +904,7 @@ int hall_pf_probe(struct platform_device *pdev)
 		AK8789_ERRMSG("hw_input_camera_hall alloc error %ld", PTR_ERR(hw_hall_dev.hw_input_camera_hall));
 		goto input_err;
 	}
+
 	hw_hall_dev.hw_input_camera_hall->name = "camera_hall";
 	set_bit(EV_KEY, hw_hall_dev.hw_input_camera_hall->evbit);
 	set_bit(BTN_TRIGGER_HAPPY, hw_hall_dev.hw_input_camera_hall->keybit);
