9b84e40f75e293b10000a428505842a2c57a55e9
Michael W <baddaemon87@gmail.com>
2018-02-11 16:19:51 +0100

    Import init from stock 5.1 sources
    Change-Id: I9f90eb9574e43ebc16acb576cfb57a7aa13b8b80

--- init/version.c
+++ init/version.c
@@ -48,3 +48,13 @@ const char linux_proc_banner[] =
 	"%s version %s"
 	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
 	" (" LINUX_COMPILER ") %s\n";
+
+#ifdef CONFIG_HUAWEI_KERNEL
+#ifndef HIDE_PRODUCT_INFO_KERNEL
+//Define the struct to mask the machine version
+const char hide_version[] =
+	"%s version %s"
+	"(android@localhost)"
+	"("LINUX_COMPILER")%s\n";
+#endif
+#endif
