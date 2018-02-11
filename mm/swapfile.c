66e95c275fab75512b5be90fa18e9fb1d249ded0
Michael W <baddaemon87@gmail.com>
2018-02-11 21:06:56 +0100

    Import mm changes from stock 5.1 sources
    Change-Id: I4c2c036cd6eb84c21fbaa04e58b95f1e4f255dd0

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -40,13 +40,18 @@
 #include <linux/swapops.h>
 #include <linux/page_cgroup.h>
 
+#ifdef CONFIG_DUMP_SYS_INFO
+#include <linux/module.h>
+#include <linux/srecorder.h>
+#endif
+
 static bool swap_count_continued(struct swap_info_struct *, pgoff_t,
 				 unsigned char);
 static void free_swap_count_continuations(struct swap_info_struct *);
 static sector_t map_swap_entry(swp_entry_t, struct block_device**);
 
 DEFINE_SPINLOCK(swap_lock);
-static unsigned int nr_swapfiles;
+unsigned int nr_swapfiles;
 atomic_long_t nr_swap_pages;
 /* protected with swap_lock. reading in vm_swap_full() doesn't need lock */
 long total_swap_pages;
@@ -67,6 +72,26 @@ static DEFINE_MUTEX(swapon_mutex);
 static DECLARE_WAIT_QUEUE_HEAD(proc_poll_wait);
 /* Activity counter to indicate that a swapon or swapoff has occurred */
 static atomic_t proc_poll_event = ATOMIC_INIT(0);
+
+#ifdef CONFIG_DUMP_SYS_INFO
+unsigned long get_nr_swapfiles(void)
+{
+    return (unsigned long)&nr_swapfiles;
+}
+EXPORT_SYMBOL(get_nr_swapfiles);
+
+unsigned long get_swap_lock(void)
+{
+    return (unsigned long)&swap_lock;
+}
+EXPORT_SYMBOL(get_swap_lock);
+
+unsigned long get_swap_info(void)
+{
+    return (unsigned long)&swap_info;
+}
+EXPORT_SYMBOL(get_swap_info);
+#endif
 
 static inline unsigned char swap_count(unsigned char ent)
 {
