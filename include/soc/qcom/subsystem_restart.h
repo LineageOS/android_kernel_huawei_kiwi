432a8fbe6514651265f30c42ced8f05de547d097
Michael W <baddaemon87@gmail.com>
2018-02-24 12:49:42 +0100

    Partially import all other driver and include changes from stock 5.1
    Change-Id: I99a9264b0be0fc5c7e023be234a03052c1a0bb3f

--- include/soc/qcom/subsystem_restart.h
+++ include/soc/qcom/subsystem_restart.h
@@ -96,6 +96,10 @@ struct notif_data {
 
 #if defined(CONFIG_MSM_SUBSYSTEM_RESTART)
 
+#ifdef CONFIG_HUAWEI_KERNEL
+extern int subsystem_restart_requested;
+#endif
+
 extern int subsys_get_restart_level(struct subsys_device *dev);
 extern int subsystem_restart_dev(struct subsys_device *dev);
 extern int subsystem_restart(const char *name);
