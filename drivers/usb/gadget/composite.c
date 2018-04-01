7a4ccfbae4158fa01cb6dfb4cb3b9464914666c5
Michael W <baddaemon87@gmail.com>
2018-04-01 18:02:35 +0200

    Partially import usb drivers from stock 5.1 and 6.0
    Change-Id: Ia855b86045885b28be854b13c091c525c559bc48

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -21,6 +21,7 @@
 #include <linux/usb/composite.h>
 #include <asm/unaligned.h>
 
+#include <chipset_common/hwusb/hw_usb_rwswitch.h>
 /*
  * The code in this file is utility code, used to build a gadget driver
  * from one or more "function" drivers, one or more "configuration"
@@ -1595,6 +1596,12 @@ composite_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *ctrl)
 			break;
 		}
 		break;
+        case USB_REQ_VENDOR_SWITCH_MODE:
+			if ((ctrl->bRequestType != (USB_DIR_IN|USB_TYPE_VENDOR|USB_RECIP_DEVICE))	|| (w_index != 0))
+				goto unknown;			/* Handle vendor customized request */
+			INFO(cdev, "vendor request: %d index: %d value: %d length: %d\n",ctrl->bRequest, w_index, w_value, w_length);
+			hw_usb_port_switch_request(INDEX_FACTORY_REWORK);
+	    break;
 	default:
 unknown:
 		VDBG(cdev,
@@ -1916,7 +1923,11 @@ composite_resume(struct usb_gadget *gadget)
 {
 	struct usb_composite_dev	*cdev = get_gadget_data(gadget);
 	struct usb_function		*f;
+#ifdef CONFIG_HUAWEI_KERNEL
+	u16				maxpower;
+#else
 	u8				maxpower;
+#endif
 	int ret;
 	unsigned long			flags;
 
